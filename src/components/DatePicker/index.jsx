db21b3e8271ac415767c3476a6a7a38f08f3e7a1
Baivab Kundu <baivab.kundu@bigbinary.com>
2025-09-02 10:51:14 +0530

    Updated handleOnCalendarChange in DatePicker to handle range type cases (#2585)
    * Updated handleOnCalendarChange in DatePicker to handle range type cases
    
    * Minor change

--- src/components/DatePicker/index.jsx
+++ src/components/DatePicker/index.jsx
@@ -104,7 +104,8 @@ const DatePicker = forwardRef(
     };
 
     const handleOnCalendarChange = date => {
-      if (!date) return;
+      if (type === "range" || !date) return;
+
       const allowed = getAllowedValue(getTimezoneAppliedDateTime(date));
       setValue(allowed);
     };
