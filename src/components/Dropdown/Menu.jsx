a54c6401c58d47650d2e76da4cb087b3853f3d7c
Labeeb Latheef <43567561+labeebklatif@users.noreply.github.com>
2025-10-31 20:22:58 +0530

    Adds keyboard navigation for Dropdown menu (#2627)
    * Adds keyboard navigation for Dropdown menu
    
    * Cleanup
    
    * Cleanup
    
    * Fixes test
    
    * Cleanup

--- src/components/Dropdown/Menu.jsx
+++ src/components/Dropdown/Menu.jsx
@@ -1,16 +1,60 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import classnames from "classnames";
 import PropTypes from "prop-types";
 
-const Menu = ({ children, className, ...otherProps }) => (
-  <ul
-    className={classnames("neeto-ui-dropdown__popup-menu", className)}
-    {...otherProps}
-  >
-    {children}
-  </ul>
-);
+const itemClassName = "neeto-ui-dropdown__popup-menu-item-btn";
+
+const Menu = ({ children, className, ...otherProps }) => {
+  const activeIndexRef = useRef(-1);
+  const menuRef = useRef(null);
+
+  const onKeyDown = event => {
+    const menu = menuRef.current;
+    const key = event.key?.toLowerCase();
+    let activeIndex = activeIndexRef.current;
+    let eventHandled = false;
+
+    const items = menu.getElementsByClassName(itemClassName);
+    const itemsCount = items.length;
+    if (itemsCount === 0) return;
+
+    if (key === "arrowdown") {
+      activeIndex = activeIndex >= itemsCount - 1 ? 0 : activeIndex + 1;
+      items[activeIndex].focus();
+      eventHandled = true;
+    } else if (key === "arrowup") {
+      activeIndex = activeIndex <= 0 ? itemsCount - 1 : activeIndex - 1;
+      items[activeIndex].focus();
+      eventHandled = true;
+    } else if (key === "enter") {
+      items[activeIndex].click();
+      eventHandled = true;
+    }
+
+    if (!eventHandled) return;
+    activeIndexRef.current = activeIndex;
+    event.preventDefault();
+    event.stopPropagation();
+  };
+
+  useEffect(() => {
+    const menu = menuRef.current;
+    if (menu) menu.focus();
+  }, [menuRef]);
+
+  return (
+    <ul
+      {...{ onKeyDown }}
+      className={classnames("neeto-ui-dropdown__popup-menu", className)}
+      ref={menuRef}
+      tabIndex={0}
+      {...otherProps}
+    >
+      {children}
+    </ul>
+  );
+};
 
 Menu.propTypes = {
   /**
