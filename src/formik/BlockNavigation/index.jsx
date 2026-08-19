43b682b686dafca1681d1e1e68f572215d2f7acd
Suvarna-Vats <suvarna.vats@pixelcompute.com>
2026-08-19 14:31:34 +0530

    Add saveAndContinue variant to BlockNavigation to save the form before navigating (#2869)
    * Add saveAndContinue variant to BlockNavigation
    
    * Await submitForm in FormWrapper to catch failed submissions
    
    * Address review comments

--- src/formik/BlockNavigation/index.jsx
+++ src/formik/BlockNavigation/index.jsx
@@ -1,14 +1,21 @@
 /* eslint-disable @bigbinary/neeto/file-name-and-export-name-standards */
-import React from "react";
+import React, { useState } from "react";
 
 import { useFormikContext } from "formik";
 import PropTypes from "prop-types";
+import { isEmpty } from "ramda";
 
 import { useNavPrompt } from "hooks";
 
 import Alert from "./Alert";
 
-const BlockNavigation = ({ isDirty = false, ...otherProps }) => {
+const BlockNavigation = ({
+  isDirty = false,
+  saveAndContinue = false,
+  ...otherProps
+}) => {
+  const [isSaving, setIsSaving] = useState(false);
+
   const formikContext = useFormikContext();
   const shouldBlock =
     isDirty || (Boolean(formikContext) && Boolean(formikContext.dirty));
@@ -23,12 +30,39 @@ const BlockNavigation = ({ isDirty = false, ...otherProps }) => {
     continueNavigation();
   };
 
+  const handleSaveAndContinue = async () => {
+    if (!formikContext) {
+      hidePrompt();
+
+      return;
+    }
+
+    setIsSaving(true);
+    try {
+      const errors = await formikContext.validateForm();
+      if (!isEmpty(errors)) {
+        await formikContext.submitForm();
+        hidePrompt();
+
+        return;
+      }
+
+      await formikContext.submitForm();
+      continueNavigation();
+    } catch {
+      hidePrompt();
+    } finally {
+      setIsSaving(false);
+    }
+  };
+
   return (
     <Alert
+      {...{ isSaving, saveAndContinue }}
       isOpen={isBlocked}
       onClose={hidePrompt}
       onDiscardChanges={handleDiscardChanges}
-      onSubmit={hidePrompt}
+      onSubmit={saveAndContinue ? handleSaveAndContinue : hidePrompt}
       {...otherProps}
     />
   );
@@ -36,6 +70,7 @@ const BlockNavigation = ({ isDirty = false, ...otherProps }) => {
 
 BlockNavigation.propTypes = {
   isDirty: PropTypes.bool,
+  saveAndContinue: PropTypes.bool,
   message: PropTypes.string,
   title: PropTypes.string,
   submitButtonLabel: PropTypes.string,
